b3e8ae8adde16b7071ab6a15fa85d08062171db9
Gonzalo Contaldo <gonzacontaldo@gmail.com>
2026-01-13 11:47:03 -0500

    Lab 1 Questions

---
+++ Week01/lab1.py
@@ -0,0 +1,15 @@
+# Sample Coding Questions 01 Week 01
+# Gonzalo Contaldo
+
+array = [1, 4, 7, 9]
+
+a, b, c, d = 1, 2, 3, 4
+
+# Given the following line of code, give the Fully-Bracketed Version of: e = a - b ** c // d + a % c
+e_fully_bracketed = a - ((b ** c) // d) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+age = int(input("Please enter your age: "))
+print("Now showing the shop items filtered by age: {}".format(age))
